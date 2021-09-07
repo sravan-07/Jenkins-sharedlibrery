1bd550d73d17bc2354569c6f9c38e11b6eaaeabe
sravan-07 <85868069+sravan-07@users.noreply.github.com>
2021-09-07 15:31:35 +0530

    Update maven.groovy

--- maven.groovy
+++ maven.groovy
@@ -1,15 +1,16 @@
+library 'sharedfile'
 node
 {
     stage('checkout')
     {
-    checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/jabedhasan21/java-hello-world-with-maven.git']]])
+        code_checkout()
     }
     stage('build')
     {
-        sh 'mvn clean install'
+       code_build()
     }
     stage('Nexus-Upload')
     {
-        nexusArtifactUploader artifacts: [[artifactId: 'jb-hello-world-maven', classifier: '', file: 'target/jb-hello-world-maven-0.1.0.jar', type: 'jar']], credentialsId: 'Nexus-ID', groupId: 'org.springframework', nexusUrl: '192.168.87.128:8082', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-releases', version: '0.1.0'
+       nexus_upload()
     }
 }
